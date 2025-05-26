46f736457c8ba997dfd706631f48ca6f07389565
SergeySudakov80 <146466165+SergeySudakov80@users.noreply.github.com>
2025-05-26 11:24:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from typing import List
 
 from fastapi import FastAPI, Depends, HTTPException
@@ -7,24 +8,20 @@ from schemas import *
 from database import engine, session
 
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def shutdown():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Recipe.metadata.create_all)
-
-
-@app.on_event("shutdown")
-async def shutdown():
+    yield
     await session.close()
     await engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post('/recipes/', response_model=BookOut)
 async def create_recipe(book: BookIn) -> Recipe:
-    new_recipe = Recipe(**book.dict())
+    new_recipe = Recipe(**book.model_dump())
     async with session.begin():
         session.add(new_recipe)
     await session.commit()
